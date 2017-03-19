a183a30104f0ccb8618128b96121fbbe4caa8293
Francois Roy <frns.roy@gmail.com>
2017-03-19 11:28:02 -0400

    Added details in the MVP specifications.

--- cmake/ReplicateSourceTree.cmake
+++ cmake/ReplicateSourceTree.cmake
@@ -1,5 +1,7 @@
 # Note: 
 # when executed in the build dir, CMAKE_CURRENT_SOURCE_DIR is the build dir.
-file( COPY images snippets tests DESTINATION "${CMAKE_ARGV3}" )
+file( COPY snippets tests DESTINATION "${CMAKE_ARGV3}" )
+file( COPY images  DESTINATION "${CMAKE_ARGV3}" 
+FILES_MATCHING PATTERN "*.png" PATTERN "*.ico" PATTERN "*.svg")
 file( COPY doc  DESTINATION "${CMAKE_ARGV3}" )
 #FILES_MATCHING PATTERN "*.py" PATTERN "*.rst" PATTERN "*.html" PATTERN "*.css")
